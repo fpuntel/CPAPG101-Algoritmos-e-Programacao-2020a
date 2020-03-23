606b15dfeea5fe72ecc386e987b4c95a52affee2
fpuntel <fernandopuntel@gmail.com>
2020-03-23 10:42:20 -0300

    23-03

---
+++ SemanaRevisao/codigo01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // Declaracao
+    int idade;
+    float peso;
+
+    // Atribuindo os valores
+    idade = 30;
+    peso = 90.5;
+
+    // %i ou %d = inteiro
+    // %f = float
+    printf("Idade: %i . Peso: %f", idade, peso);
+
+    return 0;
+}
---
+++ SemanaRevisao/soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int numero1, numero2;
+    int soma;
+
+    numero1 = 74;
+    numero2 = 25;
+
+    // Soma
+    soma = numero1 + numero2;
+
+    // Mostrar resultado
+    printf("Soma: %d \n", soma);
+
+    return 0;
+}
