f9899ff244b46e2b326a36367111c531e38c1507
Fernando Emilio Puntel <fernandopuntel@gmail.com>
2020-04-22 10:00:09 -0300

    Add files via upload

---
+++ CodigosVideoAual/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int resultado;
+
+    resultado = 42 + 77;
+
+    printf("RESULTADO : %d", resultado);
+
+    return 0;
+}
---
+++ CodigosVideoAual/soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float numero1, numero2, resultado;
+
+    numero1 = 17.0;
+    numero2 = 45.4;
+
+    resultado = numero1 + numero2;
+
+    printf("RESULTADO: %f\n", resultado);
+
+    return 0;
+}
