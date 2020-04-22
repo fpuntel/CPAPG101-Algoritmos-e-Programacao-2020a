928d330373405bed3fbfefd02382f0cba5173424
Fernando Emilio Puntel <fernandopuntel@gmail.com>
2020-04-22 09:59:47 -0300

    Create calculadora.c

---
+++ CodigosVideoAual/calculadora.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+  Algoritmo calculadora
+*/
+
+int main(){
+    // DECLARACOES
+    float adicao, subtracao, multiplicacao, divisao;
+
+    // OPERAÇÕES
+    adicao = 36 + 41;
+    subtracao = 36 - 41;
+    multiplicacao = 36 * 41;
+    divisao = 36.0 / 41.0;
+
+    // IMPRESSÃO
+    printf("ADICAO: %f \n", adicao);
+    printf("SUBTRACAO: %f \n", subtracao);
+    printf("MULTIPLICACAO: %f \n", multiplicacao);
+    printf("DIVISAO: %f \n", divisao);
+
+
+    return 0;
+}
