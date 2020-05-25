1e477980912331715951b9c8ec1aef44a2f31a16
Fernando Emilio Puntel <fernandopuntel@gmail.com>
2020-05-25 14:17:05 -0300

    Update voto.c

--- CodigosVideoAual/if-else/voto.c
+++ CodigosVideoAual/if-else/voto.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
+/*
+Queremos verificar se a pessoa é obrigada a votar ou não. Sabemos que por lei, pessoas com idade igual ou maior que 18 anos e menor que 70 são obrigadas a votar.
+*/
+
 int main(){
     int idade;
 
     printf("Digite sua idade: ");
     scanf("%d", &idade);
-
-    // TESTE
-    // 15
-    // 22
 
     if(idade >= 18 && idade < 70){
         printf("OBRIGADO A VOTAR\n");
