4f4cfdf0f640d4bf27563e3e77538abbb34fbc8e
Fernando Emilio Puntel <fernandopuntel@gmail.com>
2020-05-25 14:14:39 -0300

    Update arcondicionado.c

--- CodigosVideoAual/if-else/arcondicionado.c
+++ CodigosVideoAual/if-else/arcondicionado.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+
+/*
+Uma empresa pretende desenvolver um ar condicionado que irá ligar automaticamente. O ar condicionado irá ligar se a temperatura externa atingir mais que 30 graus OU menos que 10 graus. 
+Faça um algoritmo para recebe a temperatura atual e apresente na tela se o ar condicionado irá ou não ligar.
+*/
+
 int main(){
     float temperatura;
 
